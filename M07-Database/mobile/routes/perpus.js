0be620fd82e66d53fbcf14f8c7cfa9ef547b0689
Sio Jurnalils Pipin <siojurnalispipin@hotmail.com>
2021-06-24 17:59:08 +0700

    up

--- M07-Database/mobile/routes/perpus.js
+++ M07-Database/mobile/routes/perpus.js
@@ -4,9 +4,9 @@ const checkAuth = require("../middleware/check-auth");
 const perpusCtrl = require("../controllers/perpus");
 
 //Method
-router.get("/buku/:id", checkAuth, teknikerCtrl.cariBuku);
-router.post("/buku/", checkAuth, teknikerCtrl.tambahBuku);
-router.put("/buku/", checkAuth, teknikerCtrl.updateBuku);
-router.delete("/buku/:id", checkAuth, teknikerCtrl.hapusBuku);
+router.get("/buku/:id", checkAuth, perpusCtrl.cariBuku);
+router.post("/buku/", checkAuth, perpusCtrl.tambahBuku);
+router.put("/buku/", checkAuth, perpusCtrl.updateBuku);
+router.delete("/buku/:id", checkAuth, perpusCtrl.hapusBuku);
 
 module.exports = router;
